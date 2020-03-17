b224ec2a3a975fc1cb107a39fd047a562796680e
MahirJhaveri <mahirsjhaveri@gmail.com>
2020-03-17 16:56:43 -0400

    remove old extension

--- extension/content.js
+++
@@ -1,5 +0,0 @@
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.action.message == 'call') {
-        window.open(msg.action.url,"_self")
-    }
-  });
--- extension/popup.js
+++
@@ -1,8 +0,0 @@
-  // popup script
-  window.addEventListener('load', (event) => {
-    chrome.tabs.executeScript(null, {
-      file: 'content.js', //my content script   }, () => {
-        // connect() //this is where I call my function to establish a connection     });
-    });
-  });
-  chrome.runtime.sendMessage({clicked : true});
